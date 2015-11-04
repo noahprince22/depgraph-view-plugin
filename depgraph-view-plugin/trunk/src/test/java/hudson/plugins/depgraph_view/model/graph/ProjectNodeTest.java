5042f4592073c5f30225c503859e9328c609e2ba
Mitchell Wortman <wortman2@fa15-cs427-162.cs.illinois.edu>
2015-11-04 10:11:52 -0600

    adding simple test and attempting update

---
+++ depgraph-view-plugin/trunk/src/test/java/hudson/plugins/depgraph_view/model/graph/ProjectNodeTest.java
@@ -0,0 +1,31 @@
+package hudson.plugins.depgraph_view.model.graph;
+
+/**
+ * Created by wortman2 on 11/4/15.
+ */
+
+import static org.junit.Assert.*;
+
+import hudson.model.AbstractProject;
+import org.apache.tools.ant.Project;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ProjectNodeTest {
+//    private AbstractProject<?, ?> project;
+    private ProjectNode node;
+/*
+    @Before
+    node = new ProjectNode(project);
+*/
+
+    @Test
+    public void testTest() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void getColorTest() {
+        String nodeC
+    }
+}
