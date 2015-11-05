a367782eddd53f09c635f4453e593b51c81b2057
Mitchell Wortman <wortman2@fa15-cs427-162.cs.illinois.edu>
2015-11-04 19:36:17 -0600

    adding basic test

--- trunk/src/test/java/hudson/plugins/depgraph_view/model/graph/ProjectNodeTest.java
+++ trunk/src/test/java/hudson/plugins/depgraph_view/model/graph/ProjectNodeTest.java
@@ -7,17 +7,35 @@ package hudson.plugins.depgraph_view.model.graph;
 import static org.junit.Assert.*;
 
 import hudson.model.AbstractProject;
+import hudson.model.FreeStyleProject;
 import org.apache.tools.ant.Project;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.jvnet.hudson.test.JenkinsRule;
+
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Lists;
+import hudson.model.AbstractProject;
+import hudson.model.FreeStyleProject;
+import hudson.tasks.BuildTrigger;
+import org.junit.Rule;
+import org.junit.Test;
+import org.jvnet.hudson.test.JenkinsRule;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+
+import static hudson.plugins.depgraph_view.model.graph.ProjectNode.node;
 
 public class ProjectNodeTest {
-//    private AbstractProject<?, ?> project;
+    private FreeStyleProject project;
     private ProjectNode node;
-/*
-    @Before
-    node = new ProjectNode(project);
-*/
+
+    @Rule
+    public JenkinsRule j = new JenkinsRule();
 
     @Test
     public void testTest() {
@@ -25,7 +43,13 @@ public class ProjectNodeTest {
     }
 
     @Test
-    public void getColorTest() {
-//        String nodeC
+    public void getColorUnbuiltTest() throws IOException {
+        createProjectAndNode();
+        assertEquals("notbuilt", node.getColor());
+    }
+
+    public void createProjectAndNode() throws IOException {
+        project = j.createFreeStyleProject();
+        node = new ProjectNode(project);
     }
 }
